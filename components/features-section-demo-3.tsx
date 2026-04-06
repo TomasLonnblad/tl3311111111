67a31386a7fb1a978a89e72cd09f4b3e119a282e
TomasLonnblad <tomaslonnblad@gmail.com>
2026-04-06 10:37:14 +0200

    x

--- components/features-section-demo-3.tsx
+++ components/features-section-demo-3.tsx
@@ -100,23 +100,32 @@ const FeatureDescription = ({ children }: { children?: React.ReactNode }) => {
 
 export const SkeletonOne = () => {
   return (
-    <div className="relative flex h-full gap-10 px-2 py-8">
-      <div className="group mx-auto h-full w-full bg-white p-5 shadow-2xl dark:bg-neutral-900">
-        <div className="flex h-full w-full flex-1 flex-col space-y-2">
-          {/* TODO */}
-          <img
-            src="/linear.webp"
-            alt="header"
-            width={800}
-            height={800}
-            className="aspect-square h-full w-full rounded-sm object-cover object-left-top"
-          />
+    <div className="max-w-xs w-full">
+      <div
+        className={cn(
+          "group w-full cursor-pointer overflow-hidden relative card h-96 rounded-md shadow-xl mx-auto flex flex-col justify-end p-4 border border-transparent dark:border-neutral-800",
+          "bg-[url(https://tomaslonnblad.github.io/2.jpg)] bg-cover",
+          // Preload hover image by setting it in a pseudo-element
+          "before:bg-[url(https://tomaslonnblad.github.io/p.gif)] before:fixed before:inset-0 before:opacity-0 before:z-[-1]",
+          "hover:bg-[url(https://tomaslonnblad.github.io/p.gif)]",
+          "hover:after:content-[''] hover:after:absolute hover:after:inset-0 hover:after:bg-black hover:after:opacity-50",
+          "transition-all duration-500"
+        )}
+      >
+        <div className="text relative z-50">
+          <h1 className="font-bold text-xl md:text-3xl text-gray-50 relative">
+      
+          </h1>
+          <p className="font-normal text-base text-gray-50 relative my-4">
+  
+          </p>
         </div>
       </div>
-
-      <div className="pointer-events-none absolute inset-x-0 bottom-0 z-40 h-60 w-full bg-gradient-to-t from-white via-white to-transparent dark:from-black dark:via-black" />
-      <div className="pointer-events-none absolute inset-x-0 top-0 z-40 h-60 w-full bg-gradient-to-b from-white via-transparent to-transparent dark:from-black" />
     </div>
+
+
+
+
   );
 };
 

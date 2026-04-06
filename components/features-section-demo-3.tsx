b470e99153d66f6a5e12f0b9c93fbe2083c7e775
TomasLonnblad <tomaslonnblad@gmail.com>
2026-04-06 09:50:20 +0200

    x

--- components/features-section-demo-3.tsx
+++ components/features-section-demo-3.tsx
@@ -266,11 +266,6 @@ export const Globe = ({ className }: { className?: string }) => {
     };
   }, []);
 
-  return (
-    <canvas
-      ref={canvasRef}
-      style={{ width: 600, height: 600, maxWidth: "100%", aspectRatio: 1 }}
-      className={className}
-    />
-  );
+
+
 };
